ef661393a9bf2d06799babc1728441d3762bae97
maemon4095 <maemondecocted@gmail.com>
2022-12-26 20:53:56 +0900

    pointer test

--- logic/src/lib.rs
+++ logic/src/lib.rs
@@ -1,14 +1,38 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use once_cell::sync::Lazy;
+
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[wasm_bindgen]
+pub fn add2(x: i32, y: i32) -> i32 {
+    x + y
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+static BINARY: Lazy<Vec<u8>> = Lazy::new(|| vec![0, 1, 2, 3, 4]);
+
+#[wasm_bindgen]
+pub fn array() -> *const u8 {
+    let mut str = String::new();
+    for x in BINARY.iter() {
+        str.push(to_hex(*x));
+        str.push(to_hex((*x) >> 4));
+    }
+    log(&str);
+    return BINARY.as_ptr();
+
+    fn to_hex(num: u8) -> char {
+        let b = (num & 0xF) as u32;
+        unsafe {
+            if b < 10 {
+                char::from_u32_unchecked(('0' as u32) + b)
+            } else {
+                char::from_u32_unchecked(('A' as u32) + (b - 10))
+            }
+        }
     }
 }
